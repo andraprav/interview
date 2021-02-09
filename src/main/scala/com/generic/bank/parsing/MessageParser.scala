fd924db1837b4d3dd93dee31d79de40a9c9fb283
andraprav <andraprav@gmail.com>
2021-02-09 20:56:26 +0200

    remove deprecated ScalaObjectMapper

--- src/main/scala/com/generic/bank/parsing/MessageParser.scala
+++ src/main/scala/com/generic/bank/parsing/MessageParser.scala
@@ -1,16 +1,18 @@
 package com.generic.bank.parsing
 
 import com.generic.bank.domain.FinancialMessage
-import com.generic.bank.util.FileReader
-import com.generic.bank.util.JsonUtil.mapper
+import com.generic.bank.util.{FileReader, JsonUtil}
 
 import java.io.File
 
 trait MessageParser {
+  def parse(file: File): Either[Error, FinancialMessage]
+}
 
-  def parse(file: File): Either[Error, FinancialMessage] = {
+class JsonMessageParser extends MessageParser {
+  override def parse(file: File): Either[Error, FinancialMessage] = {
     val json = FileReader.readFile(file)
-    val financialMessage = mapper.readValue[FinancialMessage](json)
+    val financialMessage = JsonUtil.fromJson[FinancialMessage](json)
     Right(financialMessage)
   }
 }
--- src/main/scala/com/generic/bank/util/JsonUtil.scala
+++ src/main/scala/com/generic/bank/util/JsonUtil.scala
@@ -1,24 +1,28 @@
 package com.generic.bank.util
 
-import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
-import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 object JsonUtil {
-  val mapper = new ObjectMapper() with ScalaObjectMapper
-  mapper.registerModule(DefaultScalaModule)
+  private val mapper = JsonMapper.builder()
+    .addModule(DefaultScalaModule)
+    .build()
+
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
   def toJson(value: Map[Symbol, Any]): String = {
-    toJson(value map { case (k,v) => k.name -> v})
+    toJson(value map { case (k, v) => k.name -> v })
   }
 
   def toJson(value: Any): String = {
     mapper.writeValueAsString(value)
   }
 
-  def toMap[V](json:String)(implicit m: Manifest[V]) = fromJson[Map[String,V]](json)
+  def toMap[V](json: String): Map[String, V] = fromJson[Map[String, V]](json)
 
-  def fromJson[T](json: String)(implicit m : Manifest[T]): T = {
-    mapper.readValue[T](json)
+  def fromJson[T](json: String): T = {
+    mapper.readValue(json, new TypeReference[T] {})
   }
 }
