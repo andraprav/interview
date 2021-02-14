e904936d6ec4f792cab8dfa1bfacaae36c8c601f
andraprav <andraprav@gmail.com>
2021-02-14 16:47:45 +0200

    process IncomingStream and create OutgoingStream

--- src/main/scala/com/generic/bank/stream/outgoing/OutgoingStream.scala
+++ src/main/scala/com/generic/bank/stream/outgoing/OutgoingStream.scala
@@ -1,15 +1,47 @@
 package com.generic.bank.stream.outgoing
 
-import java.io.File
-
 import akka.NotUsed
 import akka.stream.scaladsl.Source
+import cats.implicits.catsSyntaxEitherId
+import com.generic.bank.domain.FinancialMessage
+import com.generic.bank.fraud.api.DefaultFraudApi
+import com.generic.bank.fraud.client.DefaultFraudClient
+import com.generic.bank.fraud.client.domain.FraudResult.Fraud
+import com.generic.bank.modules.ActorSystemModule
+import com.generic.bank.parsing.FinancialMessageParser
+
+import java.io.File
+import scala.concurrent.{ExecutionContext, Future}
 
 trait OutgoingStream {
 
-  def process(source: Source[File, NotUsed]): Nothing = {
-    val _ = source
-    ???
+}
+
+object OutgoingStream {
+  private val actorSystemModule = ActorSystemModule()
+  implicit val executionSystem: ExecutionContext = actorSystemModule.executionContext
+
+  private val messageParser = new FinancialMessageParser
+  private val fraudApi = new DefaultFraudApi(actorSystemModule)
+  private val fraudClient = new DefaultFraudClient(actorSystemModule, fraudApi)
+
+  def process(source: Source[File, NotUsed]): Source[Future[Either[Error, FinancialMessage]], NotUsed] = {
+    val outgoingStream = source.map(file => {
+      val financialMessageEither = messageParser.parse(file)
+      val financialMessage = financialMessageEither match {
+        case Left(_) => Future(Error.ParsingError.asLeft)
+        case Right(value) => getFraud(value)
+      }
+      financialMessage
+    })
+    outgoingStream
   }
 
+  private def getFraud(financialMessage: FinancialMessage): Future[Either[Error, FinancialMessage]] = {
+    val isFraudEither = fraudClient.call(financialMessage)
+    isFraudEither.map {
+      case Left(_) => Error.ClientError.asLeft
+      case Right(fraudResult) => financialMessage.copy(isFraud = Some(fraudResult.equals(Fraud))).asRight
+    }
+  }
 }
