8aaa4f504aebc577af34c871b5264eb5adb502c5
andraprav <andraprav@gmail.com>
2021-02-14 16:48:09 +0200

    process akka stream

--- src/main/scala/com/generic/bank/Main.scala
+++ src/main/scala/com/generic/bank/Main.scala
@@ -1,3 +1,34 @@
 package com.generic.bank
 
-object Main extends App {}
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Keep, Sink}
+import com.generic.bank.config.ApplicationConfig
+import com.generic.bank.domain.FinancialMessage
+import com.generic.bank.modules.ActorSystemModule
+import com.generic.bank.stream.incoming.IncomingStream
+import com.generic.bank.stream.outgoing.{Error, OutgoingStream}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object Main extends App {
+
+  private val actorSystemModule = ActorSystemModule()
+  implicit private val actorSystem: ActorSystem = actorSystemModule.actorSystem
+  implicit private val ec: ExecutionContext = actorSystemModule.executionContext
+
+  val applicationConfig = ApplicationConfig(ApplicationConfig.MessageFolder("/messages"))
+  val incomingStream = new IncomingStream(applicationConfig)
+  val source = incomingStream.source().getOrElse(null)
+
+  val outgoingStream = OutgoingStream
+  val output = outgoingStream.process(source)
+  val sink = Sink.foreach[Future[Either[Error, FinancialMessage]]](financialMessage =>
+    financialMessage.foreach {
+      case Right(value) => println(value)
+      case _ =>
+    })
+
+  val runnable = output.toMat(sink)(Keep.right)
+  output.runWith(sink)
+
+}
